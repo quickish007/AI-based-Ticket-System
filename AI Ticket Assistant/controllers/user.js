58632ec29e9d5e7629d6804695cab33417c36df3
Rushikesh Haveliwala <quickish51299@gmail.com>
2026-02-10 17:01:03 -0500

    created controlers, middlewares and route

--- AI Ticket Assistant/controllers/user.js
+++ AI Ticket Assistant/controllers/user.js
@@ -57,5 +57,55 @@ export const login = async (req, res)=> {
 }
 
 export const logout = async (req, res)=> {
-    
+    try{
+        const token = req.headers.authorization.split(" ")[1]
+        if(!token) return res.status(401).json({error: "Unauthorized"})
+        jwt.verify(token, process.env.JWT_SECRET, (err, decoded) =>{
+            if(err) return res.status(401).json({error:"Unauthorized"})
+            res.json({message: "Logged out successfully"})
+        })
+    }catch(error){
+        res.status(401).json({error: "logOut failed"})
+    }
+}
+
+
+export const updateUser = async (req,res)=> {
+    const {skills = [], role, email } = req.body;
+
+    try{
+        if(req.user?.role !== "admin"){
+            return res.status(403).json({error:"Forbidden"})
+
+        }
+        const user = await User.findOne({email})
+        if(!user) return res.status(401).json({error:"User not found"})
+
+        await User.updateOne(
+            {email}, 
+            {skills: skills.length ? skills : user.skills, role}
+        )
+        return res.json({message: "User updated successfully"})
+    }catch(error){
+            res.status(500).json({error: "Update failed",
+            details: error.message  
+        })
+    }
+}
+
+
+export const getUser = async (req,res)=> {
+    try {
+        if(req.user.role !== "admin") {
+            return res.status(403).json({error:"Forbidden"})
+        }
+
+        const users = await User.find().select("-password")
+        return res.json({users})
+
+    }catch(error){
+        res.status(500).json({error: "Failed to fetch users",
+            details: error.message  
+        })
+    }
 }
---
+++ AI Ticket Assistant/routes/user.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import { updateUser } from '../controllers/user.js'
+import { authenticate } from '../middlewares/auth.js'
+
+const router = express.Router();
+
+router.post("/update-user", authenticate, updateUser);
+router.get("/users", authenticate, getUser);
+
+
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout); 
+
+export default router;
+
